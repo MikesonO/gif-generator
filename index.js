cc04280319a3403c069c3272992d92e03a065a07
mikeson <mikeson.jnr@gmail.com>
2022-11-01 12:07:24 +0000

    Add button to generate GIF onClick

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const img = document.querySelector('img');
-fetch("https://api.giphy.com/v1/gifs/translate?api_key=XUilUesMKlBWTz8Yo6XWlgc92VHggh0e&s=cats", {
+const button = document.querySelector('button');
+
+
+button.addEventListener("click", ()=>{
+fetch("https://api.giphy.com/v1/gifs/translate?api_key=XUilUesMKlBWTz8Yo6XWlgc92VHggh0e&s=bear", {
     mode: "cors"
   })
   .then(function (response) {
@@ -7,4 +11,6 @@ fetch("https://api.giphy.com/v1/gifs/translate?api_key=XUilUesMKlBWTz8Yo6XWlgc92
   })
   .then(function (response) {
     img.src = response.data.images.original.url;
-  });
+  })
+
+});
