a279d0639190ce0eb3c286c6f0b699d45ba788a7
mikeson <mikeson.jnr@gmail.com>
2022-11-02 15:03:30 +0000

    Display error message if search fails

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const gif = document.getElementById("generated-gif");
 const button = document.querySelector('button');
 const userInput = document.getElementById("search-input");
+const errorMsg = document.getElementById("error-msg");
+
+const reset = () =>{
+  gif.src = "";
+  gif.style.border = "none";
+  errorMsg.style.display = "none";
+}
 
 button.addEventListener("click", ()=>{
+  reset();
   if(userInput.value.trim() == ""){
     location.reload();
   }
@@ -12,10 +20,13 @@ fetch(`https://api.giphy.com/v1/gifs/translate?api_key=XUilUesMKlBWTz8Yo6XWlgc92
   .then((response) => response.json())
   .then((response) => {
     gif.src = response.data.images.original.url;
+    gif.style.border = "2px solid var(--white)";
+    gif.style.display = "block";
   })
-  .catch((error) => {
-    console.error('Error:', error);
+  .catch(() => {
+    gif.style.display = "none";
+    errorMsg.style.display = "block";
+    errorMsg.textContent = `No GIFs found for ${userInput.value}`;
+    userInput.value = "";
   });
-
-  gif.style.display = "block";
 });
