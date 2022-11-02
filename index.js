0b78208a35cb48199bb4775541427b6d53bae411
mikeson <mikeson.jnr@gmail.com>
2022-11-02 21:56:03 +0000

    Refactor code using async/await

--- index.js
+++ index.js
@@ -3,30 +3,33 @@ const button = document.querySelector('button');
 const userInput = document.getElementById("search-input");
 const errorMsg = document.getElementById("error-msg");
 
-const reset = () =>{
+const reset = () => {
   gif.src = "";
   gif.style.border = "none";
   errorMsg.style.display = "none";
 }
 
-button.addEventListener("click", ()=>{
+button.addEventListener("click", () => {
   reset();
-  if(userInput.value.trim() == ""){
+  if (userInput.value.trim() == "") {
     location.reload();
+  } else {
+  getGif();
   }
-fetch(`https://api.giphy.com/v1/gifs/translate?api_key=XUilUesMKlBWTz8Yo6XWlgc92VHggh0e&s=${userInput.value}`, {
-    mode: "cors"
-  })
-  .then((response) => response.json())
-  .then((response) => {
-    gif.src = response.data.images.original.url;
+});
+
+const getGif = async () => {
+  try {
+    const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=XUilUesMKlBWTz8Yo6XWlgc92VHggh0e&s=${userInput.value}`, { mode: "cors" });
+    const gifData = await response.json();
+    gif.src = gifData.data.images.original.url;
     gif.style.border = "2px solid var(--white)";
     gif.style.display = "block";
-  })
-  .catch(() => {
+  } 
+  catch {
     gif.style.display = "none";
     errorMsg.style.display = "block";
     errorMsg.textContent = `No GIFs found for ${userInput.value}`;
     userInput.value = "";
-  });
-});
+  }
+}
